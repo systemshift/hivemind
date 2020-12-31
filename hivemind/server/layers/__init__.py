7815e40b695bacfb82e4130a25252b2eec0994b3
justheuristic <justheuristic@gmail.com>
2020-12-31 07:12:02 +0300

    update layer names

--- hivemind/server/layers/__init__.py
+++ hivemind/server/layers/__init__.py
@@ -9,12 +9,12 @@ name_to_block = {'ffn': lambda hid_dim: FeedforwardBlock(hid_dim),
                  'nop': lambda hid_dim: NopExpert(hid_dim),
                  'det_dropout': lambda hid_dim: DeterministicDropoutNetwork(hid_dim, dropout_prob=0.2),
 
-                 'gpt_initial_128tokens_voc32k': lambda hid_dim: GPTInitialBlock(
-                     d_model=hid_dim, nhead=hid_dim // 128, num_layers=1, vocab_size=32_000),
-                 'gpt_intermediate_128tokens_voc32k': lambda hid_dim: GPTIntermediateBlock(
+                 'gpt_initial_128tokens': lambda hid_dim: GPTInitialBlock(
+                     d_model=hid_dim, nhead=hid_dim // 128, num_layers=1, vocab_size=64_000),
+                 'gpt_intermediate_128tokens_': lambda hid_dim: GPTIntermediateBlock(
                      d_model=hid_dim, nhead=hid_dim // 128, num_layers=1),
-                 'gpt_final_128tokens_voc32k': lambda hid_dim: GPTFinalBlock(
-                     d_model=hid_dim, nhead=hid_dim // 128, num_layers=0, vocab_size=32_000),
+                 'gpt_final_128tokens': lambda hid_dim: GPTFinalBlock(
+                     d_model=hid_dim, nhead=hid_dim // 128, num_layers=0, vocab_size=64_000),
                  }
 
 name_to_input = {'ffn': lambda batch_size, hid_dim: torch.empty((batch_size, hid_dim)),
@@ -24,8 +24,8 @@ name_to_input = {'ffn': lambda batch_size, hid_dim: torch.empty((batch_size, hid
                  'det_dropout': lambda batch_size, hid_dim:
                  (torch.empty((batch_size, hid_dim)), torch.randint(0, 1, (batch_size, hid_dim))),
 
-                 'gpt_initial_128tokens_voc32k': lambda batch_size, hid_dim: torch.randint(0, 10, size=(batch_size, 128)),
-                 'gpt_intermediate_128tokens_voc32k': lambda batch_size, hid_dim: torch.randn(batch_size, 128, hid_dim),
-                 'gpt_final_128tokens_voc32k': lambda batch_size, hid_dim: (
+                 'gpt_initial_128tokens': lambda batch_size, hid_dim: torch.randint(0, 10, size=(batch_size, 128)),
+                 'gpt_intermediate_128tokens': lambda batch_size, hid_dim: torch.randn(batch_size, 128, hid_dim),
+                 'gpt_final_128tokens': lambda batch_size, hid_dim: (
                      torch.randn(batch_size, 128, hid_dim), torch.randint(0, 10, size=(batch_size, 128))),
                  }
